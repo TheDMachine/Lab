d8d301dfb03ca8c93d210b5b20aa4a8f323da60c
bianco9 <lbiancoh@ucenfotec.ac.cr>
2017-06-07 22:19:44 -0600

    admin-view & admin-controller

---
+++ components/users/user.controller.js
@@ -0,0 +1,61 @@
+(function(){
+  angular
+    .module('myApp')
+    .controller('userAccountCtrl', userAccountCtrl);
+    function userAccountCtrl(userService, $scope){ //se inyecta el service userService en el controlador para que se tenga acceso
+      //controlador
+      var vm = this; //binding del controlador con el html, solo en el controlador
+      $scope.showHints = true;
+      $scope.user = {
+        name: "",
+        email: "",
+        social: "123456789",
+        phone: "N/A"
+      };
+
+        vm.myDate = new Date();
+        vm.isOpen = false;
+
+      function init(){ // función que se llama así misma para indicar que sea lo primero que se ejecute
+        vm.users = userService.getUsers();
+      }init();
+
+      vm.save= function(){
+
+          var newUser = {
+          id : vm.id,
+          name : vm.name,
+          secondName : vm.secondName,
+          firstName : vm.firstName,
+          lastName : vn.lastName,
+          nationality : vm.nationality,
+          idType : vm.idType,
+          myDate : vm.myDate,
+          type : vm.type,
+          coin : vm.coin
+        }
+        console.log(newUser);
+        userService.setUsers(newUser);
+        clean();
+        init();
+
+
+      };
+      vm.update = function(puser){
+          vm.user = puser.user;
+          vm.type = puser.type;
+          vm.coin = puser.coin;
+
+
+      }
+
+      function clean(){
+        vm.user = '';
+        vm.type = '';
+        vm.coin = '';
+      }
+
+    }
+     //se establece un objeto de angular normal
+
+})();
--- index.route.js
+++ index.route.js
@@ -4,20 +4,13 @@
     .config(configuration);
     function configuration($stateProvider, $urlRouterProvider){ //stateProvider
       $stateProvider
-        .state('login',{
-          url: '/login',
-          templateUrl: 'components/users/login.view.html'
-        })
         .state('user',{
           url : '/users', //ruta del url del estado
-          templateUrl : 'components/users/users.view.html',//vista que se va a cargar para este estado
+          templateUrl : 'components/users/user.view.html',//vista que se va a cargar para este estado
           controller: 'userAccountCtrl',
           controllerAs: 'user'
         })
-        .state('product',{
-          url : '/products',
-          templateUrl: 'components/products/product.view.html'
-        })
-      $urlRouterProvider.otherwise('/login');
+
+      $urlRouterProvider.otherwise('/users');
     }
 })();
