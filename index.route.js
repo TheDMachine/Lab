fead6c07b757e6e2d0ae9c354b70282f4ff1c178
bianco9 <lbiancoh@ucenfotec.ac.cr>
2017-06-14 12:10:59 -0600

    estructure changes

--- index.route.js
+++ index.route.js
@@ -1,6 +1,6 @@
 (function(){
   angular
-    .module('myApp')
+    .module('myApp', ['ui.router','ngMessages','ngMaterial'])
     .config(configuration);
     function configuration($stateProvider, $urlRouterProvider){ //stateProvider
       $stateProvider
@@ -9,6 +9,12 @@
           templateUrl : 'components/users/user.view.html',//vista que se va a cargar para este estado
           controller: 'userAccountCtrl',
           controllerAs: 'user'
+        })
+        .state('client',{
+          url : '/client', //ruta del url del estado
+          templateUrl : 'components/client/client.view.html',//vista que se va a cargar para este estado
+          controller: 'clientAccountCtrl',
+          controllerAs: 'client'
         })
 
       $urlRouterProvider.otherwise('/users');
