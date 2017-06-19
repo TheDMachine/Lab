d44c399fb7ca4c0872a3b0832702eb769ebbfc6b
bianco9 <lbiancoh@ucenfotec.ac.cr>
2017-06-19 13:51:10 -0600

    merge charlie into development correct

--- index.route.js
+++ index.route.js
@@ -1,33 +1,3 @@
-<<<<<<< HEAD
-(function(){
-  angular
-    .module('myApp', ['ui.router','ngMessages','ngMaterial'])
-    .config(configuration);
-    function configuration($stateProvider, $urlRouterProvider){ //stateProvider
-      $stateProvider
-        .state('admin',{
-          url : '/admin', //ruta del url del estado
-          templateUrl : 'components/users/admin.view.html',//vista que se va a cargar para este estado
-          controller: 'adminAccountCtrl',
-          controllerAs: 'user'
-        })
-        .state('coach',{
-          url : '/coaches', //ruta del url del estado
-          templateUrl : 'components/coach/coach.view.html',//vista que se va a cargar para este estado
-          controller: 'coachAccounCtrl',
-          controllerAs: 'coach'
-        })
-        .state('client',{
-          url : '/client', //ruta del url del estado
-          templateUrl : 'components/client/client.view.html',//vista que se va a cargar para este estado
-          controller: 'clientAccountCtrl',
-          controllerAs: 'client'
-        })
-
-      $urlRouterProvider.otherwise('/coaches');
-    }
-})();
-=======
 (function(){
   angular
     .module('myApp', ['ui.router','ngMessages','ngMaterial','ngCookies'])
@@ -68,4 +38,3 @@
       $urlRouterProvider.otherwise('/login');
     }
 })();
->>>>>>> development
