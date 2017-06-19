25f2fac30f8d62cf94547587203e9342952e45b8
bianco9 <lbiancoh@ucenfotec.ac.cr>
2017-06-19 14:40:42 -0600

    route change

--- index.route.js
+++ index.route.js
@@ -1,33 +1,3 @@
-<<<<<<< HEAD
-(function(){
-  angular
-    .module('myApp', ['ui.router','ngMessages','ngMaterial'])
-    .config(configuration);
-    function configuration($stateProvider, $urlRouterProvider){ //stateProvider
-      $stateProvider
-        .state('admin',{
-          url : '/admin', //ruta del url del estado
-          templateUrl : 'components/users/admin.view.html',//vista que se va a cargar para este estado
-          controller: 'adminAccountCtrl',
-          controllerAs: 'user'
-        })
-        .state('coach',{
-          url : '/coaches', //ruta del url del estado
-          templateUrl : 'components/coach/coach.view.html',//vista que se va a cargar para este estado
-          controller: 'coachAccounCtrl',
-          controllerAs: 'coach'
-        })
-        .state('client',{
-          url : '/client', //ruta del url del estado
-          templateUrl : 'components/client/client.view.html',//vista que se va a cargar para este estado
-          controller: 'clientAccountCtrl',
-          controllerAs: 'client'
-        })
-
-      $urlRouterProvider.otherwise('/coaches');
-    }
-})();
-=======
 (function(){
   angular
     .module('myApp', ['ui.router','ngMessages','ngMaterial','ngCookies'])
@@ -61,11 +31,10 @@
               return $ocLazyLoad.load('./components/coaches/coach.controller.js')
             }]
           },
-          controller: 'coachAccountCtrl',
+          controller: 'coachAccounCtrl',
           controllerAs: 'coach'
         })
 
-      $urlRouterProvider.otherwise('/login');
+      $urlRouterProvider.otherwise('/coaches');
     }
 })();
->>>>>>> development
